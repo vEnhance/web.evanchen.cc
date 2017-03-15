fa0e9c351f333c8dcb0695c71498c0637a8b9f91
Evan Chen <evanchen@mit.edu>
2017-03-15 15:57:27 -0400

    Favicon data

--- sync.sh
+++ sync.sh
@@ -3,6 +3,8 @@ gsutil -m rsync -R output/ gs://web.evanchen.cc/
 gsutil -m rsync -R css/ gs://web.evanchen.cc/css/
 gsutil -m rsync -R elmo/ gs://web.evanchen.cc/elmo/
 gsutil -m rsync -R static/ gs://web.evanchen.cc/web/
+gsutil -m rsync favicon.ico gs://web.evanchen.cc/
+gsutil -m rsync -R icons gs://web.evanchen.cc/icons/
 
 echo ""
 echo "Gentle reminder: exams, handouts, upload directory not sync'ed"
