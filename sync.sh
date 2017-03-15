b966329692a47fd0836aa4250903a1fd4d59a864
Evan Chen <evanchen@mit.edu>
2017-03-15 16:19:16 -0400

    Sync private directory too

--- sync.sh
+++ sync.sh
@@ -7,4 +7,4 @@ gsutil -m rsync icons/favicon.ico gs://web.evanchen.cc/
 gsutil -m rsync -R icons gs://web.evanchen.cc/icons/
 
 echo ""
-echo "Gentle reminder: exams, handouts, upload directory not sync'ed"
+echo "Gentle reminder: exams, handouts, private, upload directory not sync'ed"
