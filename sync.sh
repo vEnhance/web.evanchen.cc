f45d972dcb5bbdf14572f80c9a3d86fa0eb538ab
Evan Chen <evanchen@mit.edu>
2017-03-15 16:13:27 -0400

    Meta description tags for pages

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@ gsutil -m rsync -R output/ gs://web.evanchen.cc/
 gsutil -m rsync -R css/ gs://web.evanchen.cc/css/
 gsutil -m rsync -R elmo/ gs://web.evanchen.cc/elmo/
 gsutil -m rsync -R static/ gs://web.evanchen.cc/web/
-gsutil -m rsync favicon.ico gs://web.evanchen.cc/
+gsutil -m rsync icons/favicon.ico gs://web.evanchen.cc/
 gsutil -m rsync -R icons gs://web.evanchen.cc/icons/
 
 echo ""
