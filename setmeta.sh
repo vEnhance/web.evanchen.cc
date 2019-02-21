c829414ab70bfab8ba6320cd07691a18859ae1ff
Evan Chen <evanchen@mit.edu>
2019-02-20 20:51:17 -0500

    Cleaner meta script

--- setmeta.sh
+++ setmeta.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-gsutil -m setmeta -R -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/
+
+if [ $# -eq 0 ]; then
+	gsutil -m setmeta -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/*.html
+elif [ $1 = "-h" ]; then
+	gsutil -m setmeta -R -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/**.html
+elif [ $1 = "-p" ]; then
+	gsutil -m setmeta -R -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/**.pdf
+elif [ $1 = "-a" ]; then
+	gsutil -m setmeta -R -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/
+else
+	echo "huh?"
+fi
