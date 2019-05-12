d7648c97cc7e2eb2f1d774a214477408a296e901
Evan Chen <evanchen@mit.edu>
2019-05-12 09:16:13 -0400

    Just don't cache

--- setmeta.sh
+++ setmeta.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	gsutil -m setmeta -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/*.html
+	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/*.html
 elif [ $1 = "-h" ]; then
-	gsutil -m setmeta -R -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/**.html
+	gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/**.html
 elif [ $1 = "-p" ]; then
-	gsutil -m setmeta -R -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/**.pdf
+	gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/**.pdf
 elif [ $1 = "-a" ]; then
-	gsutil -m setmeta -R -h 'Cache-Control:public, max-age=300, no-transform' gs://web.evanchen.cc/
+	gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/
 else
 	echo "huh?"
 fi
