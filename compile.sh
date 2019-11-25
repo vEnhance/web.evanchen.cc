0c45fec7849000760f6666ede18ade51a64202be
Evan Chen <evan@evanchen.cc>
2019-11-24 20:58:25 -0500

    Fix compile script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python2 poole/poole.py -b --base-url=//web.evanchen.cc
+
+if [ $# -eq 0 ]; then
+	python2 poole/poole.py -b --base-url=//web.evanchen.cc
+elif [ $1 = "-debug" ]; then
+	python2 poole/poole.py -b --base-url=https://web.evanchen.cc
+else
+	echo "huh?"
+fi
