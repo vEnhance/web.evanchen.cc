706202c0a6e1e03e7f01a6d7a291a7766bee9add
Evan Chen <evan@evanchen.cc>
2019-12-09 11:25:00 -0500

    Refresh permissions on private dir too

--- setmeta.sh
+++ setmeta.sh
@@ -8,6 +8,7 @@ elif [ $1 = "-p" ]; then
 	gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/**.pdf
 elif [ $1 = "-u" ]; then
 	gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/upload/*
+	gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/private/*
 elif [ $1 = "-a" ]; then
 	gsutil -m setmeta -R -h 'Cache-Control:private, max-age=0, no-transform' gs://web.evanchen.cc/
 else
