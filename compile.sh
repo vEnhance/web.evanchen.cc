eada93e4e2af8eb65d34a09101b50e92c68efabb
Evan Chen <evan@evanchen.cc>
2020-01-15 06:16:14 -0500

    Switch poole to Python 3

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	python2 poole/poole.py -b --base-url=//web.evanchen.cc
+	python poole/poole.py -b --base-url=//web.evanchen.cc
 elif [ $1 = "-debug" ]; then
-	python2 poole/poole.py -b --base-url=https://web.evanchen.cc
+	python poole/poole.py -b --base-url=https://web.evanchen.cc
 else
 	echo "huh?"
 fi
