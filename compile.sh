e163f3649e89178d04586dcf9610462a73e4c11b
Evan Chen <evan@evanchen.cc>
2020-04-28 11:01:59 -0400

    Local option for compile

--- compile.sh
+++ compile.sh
@@ -4,6 +4,8 @@ if [ $# -eq 0 ]; then
 	python poole/poole.py -b --base-url=//web.evanchen.cc
 elif [ $1 = "-debug" ]; then
 	python poole/poole.py -b --base-url=https://web.evanchen.cc
+elif [ $1 = "-local" ]; then
+	python poole/poole.py -b --base-url=file:///home/evan/Dropbox/Documents/www/
 else
 	echo "huh?"
 fi
