5fd3d1016a370a5ef6b9c1ba9214c9992cd5abdc
Evan Chen <evan@evanchen.cc>
2020-08-05 01:01:15 -0700

    Use python3
    Freakin Ubuntu

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	python poole/poole.py -b --base-url=//web.evanchen.cc
+	python3 poole/poole.py -b --base-url=//web.evanchen.cc
 elif [ $1 = "-debug" ]; then
-	python poole/poole.py -b --base-url=https://web.evanchen.cc
+	python3 poole/poole.py -b --base-url=https://web.evanchen.cc
 elif [ $1 = "-local" ]; then
-	python poole/poole.py -b --base-url=file:///home/evan/Dropbox/Documents/www/
+	python3 poole/poole.py -b --base-url=file:///home/evan/Dropbox/Documents/www/
 else
 	echo "huh?"
 fi
