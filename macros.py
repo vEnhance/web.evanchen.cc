0e7e5ea10122d799fb2c884e5192c7aa593d3a3d
Evan Chen <evan@evanchen.cc>
2021-04-27 11:43:24 -0400

    Card trick

--- macros.py
+++ macros.py
@@ -47,3 +47,32 @@ def get_twitch_table():
 	out += '</table>'
 
 	return out
+
+
+def get_card_trick():
+	option_string = r'''<option value="{value}">{value}</option>'''
+	ranks = ["...", "Ace", "2", "3", "4", "5", "6",
+			"7", "8", "9", "10", "Jack", "Queen", "King"]
+	suits = ["...", "♣Clubs", "♦Diamonds", "♥Hearts", "♠Spades"]
+	out = ''
+	for n in range(0,4):
+		out += f'<h2>Card {n+1}</h2>' + '\n'
+		out += r'<div class="container trick-container">' + '\n'
+		out += f'<select class="form-select rank-select" id="rank-{n}">' + '\n'
+		for r in ranks:
+			out += option_string.format(type='rank', name=n, value=r) + '\n'
+		out += r'</select>' + '\n'
+		out += f'<select class="form-select suit-select" id="suit-{n}">' + '\n'
+		for s in suits:
+			out += option_string.format(type='suit', name=n, value=s) + '\n'
+		out += r'</select>' + '\n'
+		out += r'</div>' + '\n'
+		out += r'<hr />' + '\n'
+			
+	out += r'<h2>Card 5</h2>' + '\n'
+	out += r'<h1 id="answer">...</h1>' + '\n'
+	out += r'<button type="button" class="btn" id="trick-button"></button>' + '\n'
+	out += r'<script type="text/javascript" src="/static/trick.js.min"></script>' + '\n'
+	out += r'<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>' + '\n'
+	out += r'<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />'
+	return out
