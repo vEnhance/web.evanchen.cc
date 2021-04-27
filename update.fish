2f54f13dfaf7f8eda5d9742f3d93ec0bd7fc6d3b
Evan Chen <evan@evanchen.cc>
2021-04-27 11:52:11 -0400

    Update fish script

--- update.fish
+++ update.fish
@@ -5,11 +5,21 @@
 git diff --quiet
 
 if test $status -eq 1
-	echo "hi"
 	git status -s
+	git status -s | grep -o "[^ ]*.mkd"
+	if test $status -eq 1
+		echo "No mkd files found"
+		exit
+	end
 	gsutil cp (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
 	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 else
+	git show HEAD
+	git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd"
+	if test $status -eq 1
+		echo "No mkd files found"
+		exit
+	end
 	gsutil cp (git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
 	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 end
