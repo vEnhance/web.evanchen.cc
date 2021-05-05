b4576290977d6fd11150258a227bad5e47f1a271
Evan Chen <evan@evanchen.cc>
2021-05-05 16:35:29 -0400

    Allow targeting other commits

--- update.fish
+++ update.fish
@@ -5,8 +5,8 @@ echo "Compiled"
 
 git diff --quiet
 
-if test $status -eq 1
-	git status -s
+if test $status -eq 1 -a -z "$argv[1]"
+	echo "Using working state..."
 	git status -s | grep -o "[^ ]*.mkd"
 	if test $status -eq 1
 		echo "No mkd files found"
@@ -15,12 +15,18 @@ if test $status -eq 1
 	gsutil -m cp (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
 	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 else
-	git show HEAD --format=oneline --name-only
-	git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd"
+	set --local commit "$argv[1]"
+	if test -z $commit
+		set commit "HEAD"
+	end
+	echo "Using commit " $commit
+	git show $commit --format=oneline --name-only | grep -o "[^ ]*.mkd"
 	if test $status -eq 1
 		echo "No mkd files found"
-		exit
+	else
+		gsutil -m cp (git show $commit --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+		gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show $commit --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 	end
-	gsutil -m cp (git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 end
+echo "Make sure no leek"
+echo $commit
