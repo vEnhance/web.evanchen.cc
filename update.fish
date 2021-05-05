66b71e79392851c7d84085f4f453804c8b42b6ad
Evan Chen <evan@evanchen.cc>
2021-05-05 16:17:41 -0400

    Use gsutil -m

--- update.fish
+++ update.fish
@@ -12,7 +12,7 @@ if test $status -eq 1
 		echo "No mkd files found"
 		exit
 	end
-	gsutil cp (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+	gsutil -m cp (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
 	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 else
 	git show HEAD --format=oneline --name-only
@@ -21,6 +21,6 @@ else
 		echo "No mkd files found"
 		exit
 	end
-	gsutil cp (git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+	gsutil -m cp (git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
 	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 end
