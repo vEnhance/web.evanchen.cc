2f07391fd1db0c6b1c44b04fda2f21c5dc7870a7
Evan Chen <evan@evanchen.cc>
2021-05-05 16:17:00 -0400

    Improve update script

--- update.fish
+++ update.fish
@@ -15,7 +15,7 @@ if test $status -eq 1
 	gsutil cp (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
 	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 else
-	git show HEAD
+	git show HEAD --format=oneline --name-only
 	git show HEAD --format=oneline --name-only | grep -o "[^ ]*.mkd"
 	if test $status -eq 1
 		echo "No mkd files found"
