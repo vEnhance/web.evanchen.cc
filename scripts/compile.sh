e4855b033cea40ed28b686655bb5b5d147294d18
Evan Chen <evan@evanchen.cc>
2021-05-29 22:13:52 -0400

    Enable some markdown extensions

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+OPTIONS="--md-ext=extra --md-ext=smarty --md-ext=sane_lists"
+
 if [ $# -eq 0 ]; then
-	python3 poole/poole.py -b --base-url=//web.evanchen.cc
+	python3 poole/poole.py -b --base-url=//web.evanchen.cc $OPTIONS
 elif [ $1 = "-debug" ]; then
-	python3 poole/poole.py -b --base-url=https://web.evanchen.cc
+	python3 poole/poole.py -b --base-url=https://web.evanchen.c $OPTIONS
 elif [ $1 = "-local" ]; then
-	python3 poole/poole.py -b --base-url=file:///home/evan/Dropbox/Documents/www/
+	python3 poole/poole.py -b --base-url=file:///home/evan/Dropbox/Documents/www/  $OPTIONS
 else
 	echo "huh?"
 fi
