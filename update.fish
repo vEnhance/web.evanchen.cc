8d74c3577644082d30c88062882791ac66c13939
Evan Chen <evan@evanchen.cc>
2021-07-30 22:33:58 -0700

    Use ripgrep
    Since I alias'ed grep to ag

--- update.fish
+++ update.fish
@@ -9,18 +9,18 @@ if test $status -eq 1 -a -z "$argv[1]"
 	echo "Using working state..."
 
 	set_color normal
-	git -c color.status=always status -s | grep -e "^" -e "[^ ]*.mkd"
+	git -c color.status=always status -s | rg -e "^" -e "[^ ]*.mkd"
 	set_color cyan
 	echo "------------------------------"
 	set_color normal
 
-	git status -s | grep -o "[^ ]*.mkd" > /dev/null
+	git status -s | rg -o "[^ ]*.mkd" > /dev/null
 	if test $status -eq 1
 		echo "No mkd files found"
 		exit
 	end
-	gsutil -m cp (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
+	gsutil -m cp (git status -s | rg -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | rg -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 else
 	set --local commit "$argv[1]"
 	if test -z "$commit"
@@ -31,16 +31,16 @@ else
 
 	set_color normal
 	git show $commit --format=oneline --name-only --color=always \
-		| grep -e "^" -e "[^ ]*.mkd"
+		| rg -e "^" -e "[^ ]*.mkd"
 	set_color cyan
 	echo "------------------------------"
 	set_color normal
 
-	git show $commit --format=oneline --name-only | grep -e "[^ ]*.mkd" > /dev/null
+	git show $commit --format=oneline --name-only | rg -e "[^ ]*.mkd" > /dev/null
 	if test $status -eq 1
 		echo "No mkd files found"
 	else
-		gsutil -m cp (git show $commit --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-		gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show $commit --format=oneline --name-only | grep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
+		gsutil -m cp (git show $commit --format=oneline --name-only | rg -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+		gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show $commit --format=oneline --name-only | rg -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 	end
 end
