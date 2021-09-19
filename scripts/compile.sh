d283ae37140b99c82bd061d6162813fe914475c9
Evan Chen <evan@evanchen.cc>
2021-09-19 11:23:32 -0400

    fix(compile): bugfix compiler script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,9 +3,9 @@
 OPTIONS="--md-ext=extra --md-ext=smarty --md-ext=sane_lists"
 
 if [ $# -eq 0 ]; then
-	python3 poole/poole.py -b --base-url=//web.evanchen.cc $OPTIONS
+	python3 poole/poole.py -b --base-url=https://web.evanchen.cc $OPTIONS
 elif [ $1 = "-debug" ]; then
-	python3 poole/poole.py -b --base-url=https://web.evanchen.c $OPTIONS
+	python3 poole/poole.py -b --base-url=https://web.evanchen.cc $OPTIONS
 elif [ $1 = "-local" ]; then
 	python3 poole/poole.py -b --base-url=file:///home/evan/Sync/www/  $OPTIONS
 else
