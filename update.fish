1b0d76b995eff2c9bb6b40a16d606c83084bc4ec
Evan Chen <evan@evanchen.cc>
2022-07-27 20:57:00 -0700

    Use egrep for update.fish for compat

--- update.fish
+++ update.fish
@@ -9,18 +9,18 @@ if test $status -eq 1 -a -z "$argv[1]"
 	echo "Using working state..."
 
 	set_color normal
-	git -c color.status=always status -s | rg -e "^" -e "[^ ]*.mkd"
+	git -c color.status=always status -s | egrep "[^ ]*.mkd"
 	set_color cyan
 	echo "------------------------------"
 	set_color normal
 
-	git status -s | ag -o "[^ ]*.mkd" > /dev/null
+	git status -s | egrep -o "[^ ]*.mkd" > /dev/null
 	if test $status -eq 1
 		echo "No mkd files found"
 		exit
 	end
-	gsutil -m cp (git status -s | ag -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | ag -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
+	gsutil -m cp (git status -s | egrep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | egrep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 else
 	set --local commit "$argv[1]"
 	if test -z "$commit"
@@ -31,16 +31,16 @@ else
 
 	set_color normal
 	git show $commit --format=oneline --name-only --color=always \
-		| ag -e "^" -e "[^ ]*.mkd"
+		| egrep -e "[^ ]*.mkd"
 	set_color cyan
 	echo "------------------------------"
 	set_color normal
 
-	git show $commit --format=oneline --name-only | ag -e "[^ ]*.mkd" > /dev/null
+	git show $commit --format=oneline --name-only | egrep "[^ ]*.mkd" > /dev/null
 	if test $status -eq 1
 		echo "No mkd files found"
 	else
-		gsutil -m cp (git show $commit --format=oneline --name-only | ag -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-		gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show $commit --format=oneline --name-only | ag -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
+		gsutil -m cp (git show $commit --format=oneline --name-only | egrep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+		gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show $commit --format=oneline --name-only | egrep -o "[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 	end
 end
