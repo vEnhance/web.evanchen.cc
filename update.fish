eadc2076a2c83b51155ea4d766a4fbea0705e634
Evan Chen <evan@evanchen.cc>
2022-11-07 16:35:56 -0500

    Switch from mkd to md

--- update.fish
+++ update.fish
@@ -9,18 +9,18 @@ if test $status -eq 1 -a -z "$argv[1]"
 	echo "Using working state..."
 
 	set_color normal
-	git -c color.status=always status -s | grep -E "input/[^ ]*.mkd"
+	git -c color.status=always status -s | grep -E "input/[^ ]*.md"
 	set_color cyan
 	echo "------------------------------"
 	set_color normal
 
-	git status -s | grep -E -o "input/[^ ]*.mkd" > /dev/null
+	git status -s | grep -E -o "input/[^ ]*.md" > /dev/null
 	if test $status -eq 1
-		echo "No mkd files found"
+		echo "No md files found"
 		exit
 	end
-	gsutil -m cp (git status -s | grep -E -o "input/[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | grep -E -o "input/[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
+	gsutil -m cp (git status -s | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+	gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git status -s | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 else
 	set --local commit "$argv[1]"
 	if test -z "$commit"
@@ -31,16 +31,16 @@ else
 
 	set_color normal
 	git show $commit --format="" --name-only --color=always \
-		| grep -E -e "input/[^ ]*.mkd"
+		| grep -E -e "input/[^ ]*.md"
 	set_color cyan
 	echo "------------------------------"
 	set_color normal
 
-	git show $commit --format="" --name-only | grep -E "input/[^ ]*.mkd" > /dev/null
+	git show $commit --format="" --name-only | grep -E "input/[^ ]*.md" > /dev/null
 	if test $status -eq 1
-		echo "No mkd files found"
+		echo "No md files found"
 	else
-		gsutil -m cp (git show $commit --format="" --name-only | grep -E -o "input/[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-		gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show $commit --format="" --name-only | grep -E -o "input/[^ ]*.mkd" | sed s/.mkd\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
+		gsutil -m cp (git show $commit --format="" --name-only | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
+		gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' (git show $commit --format="" --name-only | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 	end
 end
