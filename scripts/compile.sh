cc4d3373029953d89d454ff97ee8c7bb2395264e
Evan Chen <evan@evanchen.cc>
2022-12-26 01:20:15 -0500

    markdown: truly sane lists

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-OPTIONS="--md-ext=extra --md-ext=smarty --md-ext=sane_lists"
+OPTIONS="--md-ext=extra --md-ext=smarty --md-ext=sane_lists --md-ext=mdx_truly_sane_lists"
 
 if [ $# -eq 0 ]; then
-	python3 poole/poole.py -b --base-url=https://web.evanchen.cc $OPTIONS
-elif [ $1 = "-debug" ]; then
-	python3 poole/poole.py -b --base-url=https://web.evanchen.cc $OPTIONS
-elif [ $1 = "-local" ]; then
-	python3 poole/poole.py -b --base-url=file:///home/evan/Sync/www/  $OPTIONS
+  python3 poole/poole.py -b --base-url=https://web.evanchen.cc $OPTIONS
+elif [ "$1" = "-debug" ]; then
+  python3 poole/poole.py -b --base-url=https://web.evanchen.cc $OPTIONS
+elif [ "$1" = "-local" ]; then
+  python3 poole/poole.py -b --base-url=file:///home/evan/Sync/www/ $OPTIONS
 else
-	echo "huh?"
+  echo "huh?"
 fi
