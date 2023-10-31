92e030a45b7c73950c8db313ef44f39c045c3725
Evan Chen <evan@evanchen.cc>
2023-10-31 18:44:00 -0400

    fix: missing shellcheck quotes on freeze.sh

--- scripts/freeze.sh
+++ scripts/freeze.sh
@@ -2,23 +2,23 @@
 
 if [ $# -eq 0 ]; then
   gsutil -m setmeta -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/*.html
-elif [ $1 = "-c" ]; then
+elif [ "$1" = "-c" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/**/*.css
-elif [ $1 = "-e" ]; then
+elif [ "$1" = "-e" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/exams/*
-elif [ $1 = "-o" ]; then
+elif [ "$1" = "-o" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/elmo/*
-elif [ $1 = "-s" ]; then
+elif [ "$1" = "-s" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/static/*
-elif [ $1 = "-h" ]; then
+elif [ "$1" = "-h" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/**/*.html
-elif [ $1 = "-t" ]; then
+elif [ "$1" = "-t" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/**/*.tex
-elif [ $1 = "-p" ]; then
+elif [ "$1" = "-p" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/**/*.pdf
-elif [ $1 = "-u" ]; then
+elif [ "$1" = "-u" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/upload/** gs://web.evanchen.cc/public/** gs://web.evanchen.cc/textbooks/**
-elif [ $1 = "-a" ]; then
+elif [ "$1" = "-a" ]; then
   gsutil -m setmeta -R -h 'Cache-Control:public, max-age=3600' gs://web.evanchen.cc/
 else
   echo "huh?"
