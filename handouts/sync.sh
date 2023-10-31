59f54ce560bc69cace5f246e89d4a1aad56b9ed4
Evan Chen <evan@evanchen.cc>
2023-10-30 22:59:36 -0400

    Ignore nav/snm in sync.sh too

--- handouts/sync.sh
+++ handouts/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gsutil -m rsync -r \
-  -x '\.gitignore$|sync\.sh$|.*\.pkl$|.*\.(aux|bbl|bcf|blg|brf|fdb_latexmk|fls|log|out|pre|pytxcode|pytxmcr|pytxpyg|run\.xml|toc|synctex\.gz|jpg|png)$|.*-[0-9][0-9]?\.(asy|pdf)$' \
+  -x '\.gitignore$|sync\.sh$|.*\.pkl$|.*\.(aux|bbl|bcf|blg|brf|fdb_latexmk|fls|log|out|nav|pre|pytxcode|pytxmcr|pytxpyg|run\.xml|snm|toc|synctex\.gz|jpg|png)$|.*-[0-9][0-9]?\.(asy|pdf)$' \
   . gs://web.evanchen.cc/handouts/
