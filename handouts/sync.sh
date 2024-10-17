a2b8c3680b87b8e75e2db2191c3c2d78f49bcb42
Evan Chen <evan@evanchen.cc>
2024-10-17 12:33:05 -0400

    chore: Migrate handouts/sync.sh to gcloud storage

--- handouts/sync.sh
+++ handouts/sync.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euxo pipefail
 
-PYTHONWARNINGS="ignore" gsutil -m rsync -r \
-  -x '\.gitignore$|sync\.sh$|.*\.pkl$|.*\.(aux|bbl|bcf|blg|brf|fdb_latexmk|fls|log|out|nav|pre|pytxcode|pytxmcr|pytxpyg|run\.xml|snm|toc|synctex\.gz|jpg)$|.*-[0-9][0-9]?\.(asy|pdf)$' \
-  . gs://web.evanchen.cc/handouts/
-PYTHONWARNINGS="ignore" gsutil -m setmeta -h 'Cache-Control:private, max-age=0, no-transform' "gs://web.evanchen.cc/handouts/**"
+gcloud storage rsync --recursive \
+  --exclude '\.gitignore$|sync\.sh$|.*\.pkl$|.*\.(aux|bbl|bcf|blg|brf|fdb_latexmk|fls|log|out|nav|pre|pytxcode|pytxmcr|pytxpyg|run\.xml|snm|toc|synctex\.gz|jpg)$|.*-[0-9][0-9]?\.(asy|pdf)$' \
+  --cache-control="private,max-age=0" \
+  . gs://web.evanchen.cc/handouts
