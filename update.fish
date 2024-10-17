fd4f149f6ff2f32b9dbc83ff1aec16c6c190778c
Evan Chen <evan@evanchen.cc>
2024-10-17 12:41:18 -0400

    fix: trying to work on new update.fish

--- update.fish
+++ update.fish
@@ -18,8 +18,8 @@ if test $status -eq 1 -a -z "$argv[1]"
         echo "No md files found"
         exit
     end
-    gcloud storage cp (git status -s | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-    gcloud storage objects update --custom-metadata='Cache-Control="private,max-age=0"' (git status -s | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
+    gcloud storage cp (git status -s | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/ --cache-control="private,max-age=0"
+    # gcloud storage objects update --custom-metadata='Cache-Control="private,max-age=0"' (git status -s | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
 else
     set --local commit "$argv[1]"
     if test -z "$commit"
@@ -39,7 +39,7 @@ else
     if test $status -eq 1
         echo "No md files found"
     else
-        gcloud storage cp (git show $commit --format="" --name-only | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/
-        gcloud storage objects update --custom-metadata='Cache-Control="private,max-age=0"' (git status -s | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
+        gcloud storage cp (git show $commit --format="" --name-only | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/output/) gs://web.evanchen.cc/ --cache-control="private,max-age=0"
+        # gcloud storage objects update --custom-metadata='Cache-Control="private,max-age=0"' (git show $commit --format="" | grep -E -o "input/[^ ]*.md" | sed s/.md\$/.html/ | sed s/^input/gs:\\/\\/web.evanchen.cc/)
     end
 end
