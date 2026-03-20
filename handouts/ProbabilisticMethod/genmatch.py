89ba9be74c867307738903a341dae28c278543bf
Evan Chen <evan@evanchen.cc>
2026-03-20 15:34:14 -0400

    fix: i still have py2 scripts wtf?

--- handouts/ProbabilisticMethod/genmatch.py
+++ handouts/ProbabilisticMethod/genmatch.py
@@ -1,4 +1,4 @@
-for i in xrange(5):
-	for j in xrange(5):
-		print "draw(A[%s]--B[%s], p);" %(i,j)
-	print
+for i in range(5):
+    for j in range(5):
+        print("draw(A[%s]--B[%s], p);" % (i, j))
+    print()
--- handouts/ProbabilisticMethod/genpermtable.py
+++ handouts/ProbabilisticMethod/genpermtable.py
@@ -1,19 +1,11 @@
 import itertools
 
-letters = ('W', 'X', 'Y', 'Z')
-n=0
+letters = ("W", "X", "Y", "Z")
+n = 0
 for perm in itertools.permutations(letters):
-	n += 1
-	bold = [perm[i] == letters[i] for i in xrange(4)]
-	tup = [
-			r'\textbf{\color{red} %s}' %perm[i]
-			if bold[i]
-			else perm[i]
-			for i in xrange(4)
-			]
-	print n,
-	print '&',
-	print ' & '.join(tup),
-	print '&',
-	print bold.count(True),
-	print r'\\'
+    n += 1
+    bold = [perm[i] == letters[i] for i in range(4)]
+    tup = [
+        r"\textbf{\color{red} %s}" % perm[i] if bold[i] else perm[i] for i in range(4)
+    ]
+    print(n, "&", " & ".join(tup), "&", bold.count(True), r"\\")
