9a496bf4c40b010fd4e76734a5bfbda745f45fb7
Evan Chen <evan@evanchen.cc>
2026-04-11 19:55:13 -0400

    fix: update static/js/blog.js

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,15 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const url = "https://blog.evanchen.cc/feed/";
-  fetch("https://api.rss2json.com/v1/api.json?rss_url=" + url)
-    .then((response) => response.json())
+document.addEventListener("DOMContentLoaded", () => {
+  const url = "https://blog.evanchen.cc/feeds/all.atom.xml";
+
+  fetch(url)
+    .then((response) => response.text())
+    .then((str) => new DOMParser().parseFromString(str, "application/xml"))
     .then((data) => {
-      const pub_date = data.items[0].pubDate;
-      const title = data.items[0].title;
-      const link = data.items[0].link;
+      const entry = data.querySelector("entry");
+
+      const title = entry.querySelector("title").textContent;
+      const link = entry.querySelector("link").getAttribute("href");
+      const pub_date = entry.querySelector("updated").textContent;
+
       const cut_title =
         title.length < 45 ? title : title.substring(0, 45) + "…";
+
       document.getElementById("latest_blog_date").innerHTML =
         pub_date.substring(0, 10);
+
       const anchor = document.getElementById("latest_blog_anchor");
       anchor.href = link;
       anchor.innerHTML = cut_title;
