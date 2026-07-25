f90be68ea53d700d4e6bebecf36f968ec75ea827
Evan Chen <evan@evanchen.cc>
2026-07-24 20:27:35 -0700

    polish: exclude some other weird tmp files

--- handouts/sync.sh
+++ handouts/sync.sh
@@ -5,6 +5,6 @@ rclone sync -v . web:handouts \
   --exclude '\.gitignore' \
   --exclude 'sync.sh' \
   --exclude '*.pkl' \
-  --exclude '*.{aux,bbl,bcf,blg,brf,fdb_latexmk,fls,log,out,nav,pre,pytxcode,pytxmcr,pytxpyg,run.xml,snm,toc,synctex.gz,jpg}' \
+  --exclude '*.{aux,bbl,bcf,blg,brf,fdb_latexmk,fls,log,out,nav,pre,pytxcode,pytxmcr,pytxpyg,run.xml,snm,toc,synctex.gz,jpg,tmp,tcp,thm,tmp,tps}' \
   --exclude '*-[0-9].{asy,pdf}' \
   --exclude '*-[0-9][0-9].{asy,pdf}'
